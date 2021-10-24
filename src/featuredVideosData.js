62ad1fdc761ff387987eda5c6cca9fd1185d6761
Jean <jean.soro007@gmail.com>
2021-10-24 19:20:13 -0400

    Changed featured videos game posters

--- src/featuredVideosData.js
+++ src/featuredVideosData.js
@@ -1,21 +1,21 @@
 const featuredVideosData = [
     {
         id: 1,
-        title: 'Rainbow Six: Siege: Operation Steel Wave Reveal',
+        title: 'Watchdogs: Legion',
         poster:
-            './assets/rainbow_six-2.jpeg'
+            './assets/featured-videos-sample-1.jpg'
     },
     {
         id: 2,
-        title: `Assasin's Creed Valhalla: Cinematic World Premiere`,
+        title: `Assasin's Creed Valhalla: Cinematic World Premiere Trailer`,
         poster:
             './assets/featured-videos-sample-2.jpg',
     },
     {
         id: 3,
-        title: `Rabbids Coding: Free Lessons Through Puzzle Solving`,
+        title: `Immortals Fennyx Rising`,
         poster:
-            './assets/rabbids-coding.jpeg',
+            './assets/featured-videos-sample-3.jpg',
     }
 
 ];
